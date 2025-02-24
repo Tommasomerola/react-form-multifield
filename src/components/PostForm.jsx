f7794f40ca1d9f056390c76c4effe11be320d117
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-24 18:14:12 +0100

    fai post form + stile

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -75,17 +75,82 @@ const initialPosts = [
     }
 ];
 
+const initialFormData = {
+    title: "",
+    autore: "",
+    contenuto: "",
+    categoria: "",
+};
+
 const PostForm = () => {
 
     // utilizzo dello useState per la gestione dei data (array di oggetti post)
     const [posts, setPosts] = useState(initialPosts);
     // state per la gestione delle informazioni raccolte dai campi del form
-    // const [formData, setFormData] = useState(initialFormData)
+    const [formData, setFormData] = useState(initialFormData)
+
+    // funzione di gestione delle info dei campi
+    function handleFormData(e) {
+        setFormData((currentFormData) => ({
+            ...currentFormData,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    // funzione di gestione dell'invio dell'intero form 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        // aggiungo il nuovo post alla lista      possiamo usare id: Date.now() per un id univoco
+        setPosts((currentPosts) => [...currentPosts, { id: currentPosts[currentPosts.length - 1].id + 1, ...formData }]);
+        // resetto il form
+
+        setFormData(initialFormData);
+
+    }
 
     // contenuto della pagina
     return (
         <>
-            <h1>questo e il form delle pizze</h1>
+           <h1>questo è il form delle pizze</h1>
+
+
+            <form id="formPost" action="#" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={handleFormData}
+                    value={formData.title}
+                    placeholder="Inserisci titolo post"
+                />
+
+                <input
+                    type="text"
+                    name="autore"
+                    onChange={handleFormData}
+                    value={formData.autore}
+                    placeholder="Nome autore"
+                />
+
+                <textarea
+                    name="contenuto"
+                    onChange={handleFormData}
+                    value={formData.contenuto}
+                    placeholder="Contenuto post"
+                ></textarea>
+
+                <input
+                    type="text"
+                    name="categoria"
+                    onChange={handleFormData}
+                    value={formData.categoria}
+                    placeholder="Categoria post"
+                />
+
+                {/* bottone di invio */}
+                <button>Aggiungi</button>
+
+            </form>
 
             {posts.map((post) => (
                 <div className="postItem" key={post.id}>
